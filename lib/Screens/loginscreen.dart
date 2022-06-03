d070270cc259b715963708d32c07c395ecf29dc1
Sarita-jayswal <sarita.jaiswal2073@gmail.com>
2022-06-03 16:22:23 +0545

    Rider Login page ui complete

--- lib/Screens/loginscreen.dart
+++ lib/Screens/loginscreen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -14,71 +15,91 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          SizedBox(height: 20.0),
-          Image(
-            image: AssetImage("images/logo.png"),
-            width: 350.0,
-            height: 300.0,
-            alignment: Alignment.center,
-          ),
-          Text(
-            "Login as a Rider",
-            style: TextStyle(
-              fontSize: 24,
-              fontFamily: "Brand Bold",
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20.0),
+            Image(
+              image: AssetImage("images/logo.png"),
+              width: 350.0,
+              height: 300.0,
+              alignment: Alignment.center,
             ),
-            textAlign: TextAlign.center,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      labelText: "Email",
-                      labelStyle: TextStyle(fontSize: 14),
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 10,
-                      )),
-                  style: TextStyle(fontSize: 14),
-                ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: "Password",
-                      labelStyle: TextStyle(fontSize: 14),
-                      hintStyle: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 10,
-                      )),
-                  style: TextStyle(fontSize: 14),
-                ),
-                SizedBox(height: 10),
-                MaterialButton(
-                  onPressed: () {},
-                  color: Colors.yellow,
-                  textColor: Colors.white,
-                  child: Container(
-                    height: 50,
-                    child: Center(
-                      child: Text(
-                        "Login",
-                        style:
-                            TextStyle(fontSize: 18, fontFamily: "Brand Bold"),
+            Text(
+              "Login as a Rider",
+              style: TextStyle(
+                fontSize: 24,
+                fontFamily: "Brand Bold",
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(fontSize: 14),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10,
+                        )),
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(fontSize: 14),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10,
+                        )),
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  SizedBox(height: 10),
+                  MaterialButton(
+                    onPressed: () {},
+                    color: Colors.yellow,
+                    textColor: Colors.black,
+                    child: Container(
+                      height: 50,
+                      child: Center(
+                        child: Text(
+                          "Login",
+                          style:
+                              TextStyle(fontSize: 18, fontFamily: "Brand Bold"),
+                        ),
                       ),
                     ),
-                  ),
-                  shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(24)),
-                )
-              ],
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(24)),
+                  )
+                ],
+              ),
             ),
-          )
-        ],
+            RichText(
+              text: TextSpan(
+                  text: "Don't have an account?",
+                  style:
+                      TextStyle(color: Colors.black, fontFamily: "Brand Bold"),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'Register now!',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 211, 194, 41),
+                          fontSize: 16),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          print("rEGISTERED");
+                        },
+                    )
+                  ]),
+            )
+          ],
+        ),
       ),
     );
   }
